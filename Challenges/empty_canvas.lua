f4becdf3ae280d164186c2ee1dbd9fda1429dbeb
OceanRamen <wt557978@gmail.com>
2024-06-26 02:04:59 +0100

    Feat(Challenge: Empty Canvas): Added Empty Canvas. - base 0 handsize - start with Bean, Juggler, Troubadour - Paintbrush/Pallete BANNED - start with negative showman (not eternal, just for the option) Designed by CampfireCollective

---
+++ Challenges/empty_canvas.lua
@@ -0,0 +1,33 @@
+local Challenge = {}
+Challenge.NAME = "Empty Canvas"
+Challenge.DESIGNER = "CampfireCollective"
+Challenge.DATE_CREATED = 240626
+Challenge.VERSION = "1.0.0"
+Challenge.DATA = {
+  rules = {
+    custom = {
+    },
+    modifiers = {
+      { id = "hand_size", value = 0 },
+    },
+  },
+  jokers = {
+    {id = 'j_juggler'},
+    {id = 'j_troubadour'},
+    {id = 'j_turtle_bean'},
+    { id = "j_ring_master", edition = 'negative'}
+  },
+  deck = {
+    type = "Challenge Deck",
+  },
+  restrictions = {
+    banned_cards = {
+      {id = "v_paint_brush"},
+      {id = "v_palette"},
+    },
+    banned_other = {
+    },
+  },
+}
+
+return Challenge
