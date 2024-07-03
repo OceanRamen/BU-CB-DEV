3bcf6f1921adde407fafe800fee5e8de28eacf43
OceanRamen <wt557978@gmail.com>
2024-07-03 13:39:56 +0100

    Feat(Challenge: Midterm): Renamed challenge from 'Test Stake' to 'Midterm'

---
+++ Challenges/stake_midterm.lua
@@ -0,0 +1,36 @@
+local Challenge = {}
+Challenge.NAME = "Midterm"
+Challenge.DESIGNER = "Djynasty"
+Challenge.DATE_CREATED = 240614 -- Y/M/D
+Challenge.VERSION = "1.0.0"
+Challenge.DATA = {
+  rules = {
+    custom = {
+      {
+        id = "cm_scaling",
+        value = {
+          300,
+          1800,
+          5760,
+          16200,
+          45000,
+          108000,
+          198000,
+          360000,
+        },
+      },
+      { id = "enable_rentals_in_shop" },
+      { id = "enable_eternals_in_shop" },
+      { id = "enable_perishables_in_shop" },
+      {id = 'no_reward_specific', value = 'Small'},
+    },
+    modifiers = {
+      { id = "discards", value = 2 },
+    },
+  },
+  deck = {
+    type = "Abandoned Deck",
+  },
+}
+
+return Challenge
